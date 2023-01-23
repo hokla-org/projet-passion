4125d1273a3bd5d218079e3996947cdaf714fb8c
Domitille <dommergues.domitille@gmail.com>
2023-01-23 18:36:59 +0100

    feat(chore): creating the repo structure

---
+++ app.js
@@ -0,0 +1,15 @@
+import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
+import { serveFile } from "https://deno.land/std@0.171.0/http/file_server.ts";
+import { existsSync } from "https://deno.land/std@0.171.0/fs/exists.ts";
+
+export const handleRequest = async (request) => {
+  const url = new URL(request.url);
+  const path = `${Deno.cwd()}/frontend/static${url.pathname}`;
+  if (!request.url.endsWith("/") && existsSync(path)) {
+    return await serveFile(request, path);
+  } else {
+    return new Response("Not found", { status: 404 });
+  }
+};
+
+serve(handleRequest, { port: 7777 });
